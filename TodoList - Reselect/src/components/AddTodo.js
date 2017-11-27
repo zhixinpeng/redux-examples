56cbdc0dd140b5400ef458e530f227e2210fc1df
Angus <pengzhixin@live.cn>
2017-11-27 17:14:59 +0800

    update TodoList - Reselect
    利用Reselect库创建可记忆的、可组合的selector函数。Reselct selector可以高效的计算Redux store里的衍生数据

---
+++ TodoList - Reselect/src/components/AddTodo.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class AddTodo extends Component {
+  handleClick(e) {
+    const node = this.refs.input;
+    const text = node.value.trim();
+    this.props.onAddClick(text);
+    node.value = "";
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref="input" />
+        <button onClick={e => this.handleClick(e)}>Add</button>
+      </div>
+    );
+  }
+}
+
+AddTodo.propTypes = {
+  onAddClick: PropTypes.func.isRequired
+};
